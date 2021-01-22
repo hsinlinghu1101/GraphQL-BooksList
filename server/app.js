e980609b5d54416a553c4f808b4943c70e4d6136
Hsin Ling Hu <hsinkava11@gmail.com>
2021-01-22 07:57:15 -0800

    add env

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const graphqlHTTP= require("express-graphql").graphqlHTTP;
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema.js")
 const mongoose = require('mongoose')
+const {
+    port,
+    mongoDB_Url
+} = require('./config');
 
 const app = express()
 mongoose.connect(
-    "mongodb+srv://Hsinling:Mm1101@cluster0.st9mz.mongodb.net/Cluster0?retryWrites=true&w=majority",
-     { useNewUrlParser: true, useUnifiedTopology: true }
+    mongoDB_Url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
 )
-mongoose.connection.once('open', ()=>{
+mongoose.connection.once('open', () => {
     console.log('connected to database')
 })
 
@@ -17,6 +23,6 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(4000, ()=>{
-    console.log('now listening for requests on port 4000')
+app.listen(port, () => {
+    console.log(`now listening for requests on port ${port}` )
 })
